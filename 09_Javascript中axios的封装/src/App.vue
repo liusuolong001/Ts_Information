3d67c08103521e6063cce235727beebf84440916
liusuolong001 <2531134419@qq.com>
2024-07-12 20:15:07 +0800

    edit interface

--- 09_Javascript中axios的封装/src/App.vue
+++ 09_Javascript中axios的封装/src/App.vue
@@ -3,7 +3,7 @@
  * @Author: liusuolong001
  * @Date: 2024-07-12 15:51:28
  * @LastEditors: liusuolong001
- * @LastEditTime: 2024-07-12 19:47:24
+ * @LastEditTime: 2024-07-12 20:13:48
 -->
 <script setup>
 import { ref, onMounted } from 'vue'
@@ -11,20 +11,14 @@ import { getVueApiMethod } from '@/api/vue'
 import { getReactApiMethod } from '@/api/react'
 const message = ref('message')
 
-onMounted(() => {
+onMounted(async () => {
   /* vue BaseUrl */
-  getVueApiMethod()
-    .then((result) => {
-      console.log('resultVue', result)
-    })
-    .catch((err) => {})
+  const resultVue = await getVueApiMethod()
+  console.log('resultVue', resultVue)
 
   /* react BaseUrl */
-  getReactApiMethod()
-    .then((result) => {
-      console.log('resultReact', result)
-    })
-    .catch((err) => {})
+  const result = await getReactApiMethod()
+  console.log('resultReact', resultReact)
 })
 </script>
 
